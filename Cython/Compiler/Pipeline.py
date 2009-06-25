46ea8d0217f0c934cd1f2c67899074aed7e41b2d
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2009-06-25 22:09:54 +0200

    Refactored pipeline construction to Pipeline.py

---
+++ Cython/Compiler/Pipeline.py
@@ -0,0 +1,151 @@
+import Errors
+
+#
+# Really small pipeline stages
+#
+def dumptree(t):
+    # For quick debugging in pipelines
+    print t.dump()
+    return t
+
+def abort_on_errors(node):
+    # Stop the pipeline if there are any errors.
+    if Errors.num_errors != 0:
+        raise Errors.InternalError, "abort"
+    return node
+
+def create_parse(context):
+    def parse(compsrc):
+        source_desc = compsrc.source_desc
+        full_module_name = compsrc.full_module_name
+        initial_pos = (source_desc, 1, 0)
+        scope = context.find_module(full_module_name, pos = initial_pos, need_pxd = 0)
+        tree = context.parse(source_desc, scope, pxd = 0, full_module_name = full_module_name)
+        tree.compilation_source = compsrc
+        tree.scope = scope
+        tree.is_pxd = False
+        return tree
+    return parse
+
+#
+# Pipeline factories
+#
+
+def create_pipeline(context, pxd, py=False):
+    from Visitor import PrintTree
+    from ParseTreeTransforms import WithTransform, NormalizeTree, PostParse, PxdPostParse
+    from ParseTreeTransforms import AnalyseDeclarationsTransform, AnalyseExpressionsTransform
+    from ParseTreeTransforms import CreateClosureClasses, MarkClosureVisitor, DecoratorTransform
+    from ParseTreeTransforms import InterpretCompilerDirectives, TransformBuiltinMethods
+    from TypeInference import MarkAssignments
+    from ParseTreeTransforms import AlignFunctionDefinitions, GilCheck
+    from AnalysedTreeTransforms import AutoTestDictTransform
+    from AutoDocTransforms import EmbedSignature
+    from Optimize import FlattenInListTransform, SwitchTransform, IterationTransform
+    from Optimize import OptimizeBuiltinCalls, ConstantFolding, FinalOptimizePhase
+    from Optimize import DropRefcountingTransform
+    from Buffer import IntroduceBufferAuxiliaryVars
+    from ModuleNode import check_c_declarations, check_c_declarations_pxd
+
+    # Temporary hack that can be used to ensure that all result_code's
+    # are generated at code generation time.
+    import Visitor
+    class ClearResultCodes(Visitor.CythonTransform):
+        def visit_ExprNode(context, node):
+            context.visitchildren(node)
+            node.result_code = "<cleared>"
+            return node
+
+    if pxd:
+        _check_c_declarations = check_c_declarations_pxd
+        _specific_post_parse = PxdPostParse(context)
+    else:
+        _check_c_declarations = check_c_declarations
+        _specific_post_parse = None
+
+    if py and not pxd:
+        _align_function_definitions = AlignFunctionDefinitions(context)
+    else:
+        _align_function_definitions = None
+
+    return [
+        NormalizeTree(context),
+        PostParse(context),
+        _specific_post_parse,
+        InterpretCompilerDirectives(context, context.compiler_directives),
+        _align_function_definitions,
+        ConstantFolding(),
+        FlattenInListTransform(),
+        WithTransform(context),
+        DecoratorTransform(context),
+        AnalyseDeclarationsTransform(context),
+        AutoTestDictTransform(context),
+        EmbedSignature(context),
+        MarkAssignments(context),
+        TransformBuiltinMethods(context),
+        IntroduceBufferAuxiliaryVars(context),
+        _check_c_declarations,
+        AnalyseExpressionsTransform(context),
+        OptimizeBuiltinCalls(context),
+        IterationTransform(),
+        SwitchTransform(),
+        DropRefcountingTransform(),
+        FinalOptimizePhase(context),
+        GilCheck(),
+#        ClearResultCodes(context),
+#        SpecialFunctions(context),
+        #        CreateClosureClasses(context),
+        ]
+
+def create_pyx_pipeline(context, options, result, py=False):
+    def generate_pyx_code(module_node):
+        module_node.process_implementation(options, result)
+        result.compilation_source = module_node.compilation_source
+        return result
+
+    def inject_pxd_code(module_node):
+        from textwrap import dedent
+        stats = module_node.body.stats
+        for name, (statlistnode, scope) in context.pxds.iteritems():
+            # Copy over function nodes to the module
+            # (this seems strange -- I believe the right concept is to split
+            # ModuleNode into a ModuleNode and a CodeGenerator, and tell that
+            # CodeGenerator to generate code both from the pyx and pxd ModuleNodes.
+             stats.append(statlistnode)
+             # Until utility code is moved to code generation phase everywhere,
+             # we need to copy it over to the main scope
+             module_node.scope.utility_code_list.extend(scope.utility_code_list)
+        return module_node
+
+    test_support = []
+    if options.evaluate_tree_assertions:
+        from Cython.TestUtils import TreeAssertVisitor
+        test_support.append(TreeAssertVisitor())
+
+    return ([
+            create_parse(context),
+        ] + create_pipeline(context, pxd=False, py=py) + test_support + [
+            inject_pxd_code,
+            abort_on_errors,
+            generate_pyx_code,
+        ])
+
+def create_pxd_pipeline(context, scope, module_name):
+    def parse_pxd(source_desc):
+        tree = context.parse(source_desc, scope, pxd=True,
+                          full_module_name=module_name)
+        tree.scope = scope
+        tree.is_pxd = True
+        return tree
+
+    from CodeGeneration import ExtractPxdCode
+
+    # The pxd pipeline ends up with a CCodeWriter containing the
+    # code of the pxd, as well as a pxd scope.
+    return [parse_pxd] + create_pipeline(context, pxd=True) + [
+        ExtractPxdCode(context),
+        ]
+
+def create_py_pipeline(context, options, result):
+    return create_pyx_pipeline(context, options, result, py=True)
+
